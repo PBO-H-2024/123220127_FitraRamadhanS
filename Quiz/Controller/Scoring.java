fdb5814ec050daf1818e9069d99481542546b0b0
fnt4sia <fitraramadhan668@gmail.com>
2024-03-22 16:23:16 +0700

    feat : error handling calculate

--- Quiz/Controller/Scoring.java
+++ Quiz/Controller/Scoring.java
@@ -13,46 +13,49 @@ public class Scoring {
 
         scoringGUI.submitButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e){
-
-                if(scoringGui.getNIMTextField().getText().isEmpty()){
-                    scoringGUI.setResultTextField("Please Input Your Name");
-                    return;
+                try{
+                    if(scoringGui.getNameTextField().getText().isEmpty()){
+                        scoringGUI.setResultTextField("Please Input Your Name");
+                        return;
+                    }
+    
+                    if(scoringGUI.getNIMTextField().getText().isEmpty()){
+                        scoringGui.setResultTextField("Please Input Your NIM");
+                        return;
+                    }
+                    
+                    if(!scoringGui.getRadioMobile().isSelected() && !scoringGui.getRadioWeb().isSelected()){
+                        scoringGui.setResultTextField("Please Choouse Division");
+                        return;
+                    }
+    
+                    if(scoringGui.getWritingTextField().getText().isEmpty() || scoringGui.getInterviewTextField().getText().isEmpty() || scoringGui.getCodingTextField().getText().isEmpty()){
+                        scoringGui.setResultTextField("Please Fill The Value");
+                        return;
+                    }
+    
+                    int writingScore = Integer.parseInt(scoringGui.getWritingTextField().getText());
+                    int codingScore = Integer.parseInt(scoringGui.getCodingTextField().getText());
+                    int interviewScore = Integer.parseInt(scoringGui.getInterviewTextField().getText());
+    
+                    double finalScore;
+    
+    
+                    if(scoringGui.getRadioMobile().isSelected()){
+                        finalScore = (writingScore * 0.2) + (codingScore * 0.5) + (interviewScore * 0.3); 
+                    }else{
+                        finalScore = (writingScore * 0.4) + (codingScore * 0.35) + (interviewScore * 0.25);
+                    }
+    
+                    if(finalScore >= 85){
+                        scoringGui.setResultTextField("ACCEPTED");
+                    }else{
+                        scoringGui.setResultTextField("NOT ACCEPTED");
+                    }
+                }catch(Exception error){
+                    scoringGui.setResultTextField("Error Happened When Calculating");
                 }
-
-                if(scoringGUI.getNameTextField().getText().isEmpty()){
-                    scoringGui.setResultTextField("Please Input Your NIM");
-                    return;
-                }
-                
-                if(!scoringGui.getRadioMobile().isSelected() && !scoringGui.getRadioWeb().isSelected()){
-                    scoringGui.setResultTextField("Please Choouse Division");
-                    return;
-                }
-
-                if(scoringGui.getWritingTextField().getText().isEmpty() || scoringGui.getInterviewTextField().getText().isEmpty() || scoringGui.getCodingTextField().getText().isEmpty()){
-                    scoringGui.setResultTextField("Please Fill The Value");
-                    return;
-                }
-
-                int writingScore = Integer.parseInt(scoringGui.getWritingTextField().getText());
-                int codingScore = Integer.parseInt(scoringGui.getCodingTextField().getText());
-                int interviewScore = Integer.parseInt(scoringGui.getInterviewTextField().getText());
-
-                double finalScore;
-
-
-                if(scoringGui.getRadioMobile().isSelected()){
-                    finalScore = (writingScore * 0.2) + (codingScore * 0.5) + (interviewScore * 0.3); 
-                }else{
-                    finalScore = (writingScore * 0.4) + (codingScore * 0.35) + (interviewScore * 0.25);
-                }
-
-                if(finalScore >= 85){
-                    scoringGui.setResultTextField("ACCEPTED");
-                }else{
-                    scoringGui.setResultTextField("NOT ACCEPTED");
-                }
-            }
+            };
         });
     }
 }
